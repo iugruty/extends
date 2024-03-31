a516456e67496ee4e951a7be14b5c68dce783d7a
iugruty <3022089647@qq.com>
2024-03-31 23:38:14 +0800

    about https://htmlcheatsheet.com/jquery/   remove advertisement

---
+++ removeAdvertisement/content-scripts/content.js
@@ -0,0 +1,31 @@
+/*
+var adsOne = document.querySelector('#gridId4');
+adsOne.style.cssText = 'position: absolute; left: 0px; top: -35px; z-index: 600; height: 20px; display: none; transition-property: opacity, transform; transition-duration: 0.4s; transform: translate3d(705.291px, 520.667px, 0px);'
+
+var ads5 = document.querySelector('#gridId5');
+ads5.style.cssText = 'position: absolute; left: 1071px; top: 485px;';
+
+var ads6 = document.querySelector('#gridId6');
+ads6.style.cssText = 'position: absolute; left: 0px; top: 529px;';
+
+var ads7 = document.querySelector('#gridId7');
+ads7.style.cssText = 'position: absolute; left: 535px; top: 550px;';
+
+var ads8 = document.querySelector('#gridId8');
+ads8.style.cssText = 'position: absolute; left: 0px; top: -35px; z-index: 600; height: 20px; display: none; transition-property: opacity, transform; transition-duration: 0.4s; transform: translate3d(352.646px, 1008px, 0px);';
+
+var ads9 = document.querySelector('#gridId9');
+ads9.style.cssText = 'position: absolute; left: 535px; top: 973px;';
+
+var ads10 = document.querySelector('#gridId10');
+ads10.style.cssText = 'position: absolute; left: 0px; top: -35px; z-index: 600; height: 20px; display: none; transition-property: opacity, transform; transition-duration: 0.4s; transform: translate3d(0px, 1347.67px, 0px);';
+*/
+
+
+var node4 = document.getElementById('gridId4');
+var node8 = document.getElementById('gridId8');
+var node10 = document.getElementById('gridId10');
+
+node4.remove();
+node8.remove();
+node10.remove();
